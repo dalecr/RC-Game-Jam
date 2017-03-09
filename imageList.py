2f684a4bb2fa7c80bb348681378487d029b1f1c7
Elias Dorneles <eliasdorneles@gmail.com>
2017-03-09 09:48:05 -0500

    replaces tabs by 4 spaces

--- imageList.py
+++ imageList.py
@@ -3,100 +3,100 @@
 # Created 2/24/2017 by Connor Dale
 
 class Node:
-	# A Node class that holds data and a pointer to the next node in the linked list
+    # A Node class that holds data and a pointer to the next node in the linked list
 
-	def __init__(self,data,next):
-		'''
-		Creates a Node object
-		Inputs:
-			data ------ the information stored in the node
-			next ------ the next node in the list
-		Outputs:
-			None
-		'''
-		self.data = data
-		self.next = next
-		__slots__ = [self.data, self.next]
+    def __init__(self,data,next):
+        '''
+        Creates a Node object
+        Inputs:
+            data ------ the information stored in the node
+            next ------ the next node in the list
+        Outputs:
+            None
+        '''
+        self.data = data
+        self.next = next
+        __slots__ = [self.data, self.next]
 
 
 class CircularLinkedList:
-	# A circular linked list class that stores data as a chain of Node objects
-	
-	def __init__(self):
-		'''
-		Creates a CircularLinkedList object
-		Inputs:
-			None
-		Outputs:
-			None
-		'''
-		self.last = None  # the last node in the list
-		self.current = None # the current player
+    # A circular linked list class that stores data as a chain of Node objects
+    
+    def __init__(self):
+        '''
+        Creates a CircularLinkedList object
+        Inputs:
+            None
+        Outputs:
+            None
+        '''
+        self.last = None  # the last node in the list
+        self.current = None # the current player
 
 
-	def append(self,data):
-		'''
-		Adds a node with the given data to the end of the list
-		Inputs:
-			data --------- the information to be stored
-		Outputs:
-			None
-		'''
-		new_node = Node(data,None)  # node to be added
+    def append(self,data):
+        '''
+        Adds a node with the given data to the end of the list
+        Inputs:
+            data --------- the information to be stored
+        Outputs:
+            None
+        '''
+        new_node = Node(data,None)  # node to be added
 
-		if self.is_empty():
-			self.last = new_node
-			new_node.next = new_node
-		else:
-			new_node.next = self.last.next  # new node links to first node
-			self.last.next = new_node  # previous last node links to new node
-			self.last = new_node  # new node gets last-node pointer 
-
-
-	def set_current(self,data=None):
-		'''
-		Searches the list for a node containing the specified value, then 
-		sets the current player marker
-		Inputs:
-			data --------- the value in the list that is searched for
-		Outputs:
-			None
-		'''
-		if not self.is_empty():
-			if data == None:
-				self.current = self.last
-				return
-			marker = self.last
-			while True:
-				marker = marker.next
-				if marker.data == data:
-					self.current = marker
-				if marker == self.last:
-					return
+        if self.is_empty():
+            self.last = new_node
+            new_node.next = new_node
+        else:
+            new_node.next = self.last.next  # new node links to first node
+            self.last.next = new_node  # previous last node links to new node
+            self.last = new_node  # new node gets last-node pointer 
 
 
-	def update_current(self):
-		'''
-		Sets the current player marker to the next in the list
-		Inputs:
-			None
-		Outputs:
-			None
-		'''
-		if self.current != None:
-			self.current = self.current.next
+    def set_current(self,data=None):
+        '''
+        Searches the list for a node containing the specified value, then 
+        sets the current player marker
+        Inputs:
+            data --------- the value in the list that is searched for
+        Outputs:
+            None
+        '''
+        if not self.is_empty():
+            if data == None:
+                self.current = self.last
+                return
+            marker = self.last
+            while True:
+                marker = marker.next
+                if marker.data == data:
+                    self.current = marker
+                if marker == self.last:
+                    return
 
 
-	def is_empty(self):
-			'''
-			Checks whether there is a value assigned to self.last
-			to determine whether there are any elements in the list
-			Inputs:
-				None
-			Outputs:
-				True ------ if list is empty
-				False ----- if it isn't
-			'''
-			if self.last == None:
-				return True
-			return False
+    def update_current(self):
+        '''
+        Sets the current player marker to the next in the list
+        Inputs:
+            None
+        Outputs:
+            None
+        '''
+        if self.current != None:
+            self.current = self.current.next
+
+
+    def is_empty(self):
+            '''
+            Checks whether there is a value assigned to self.last
+            to determine whether there are any elements in the list
+            Inputs:
+                None
+            Outputs:
+                True ------ if list is empty
+                False ----- if it isn't
+            '''
+            if self.last == None:
+                return True
+            return False
--- octopus.py
+++ octopus.py
@@ -17,248 +17,248 @@ BLUE = (0, 0, 255)
 class Octopus(object):
 
 
-	def __init__(self,win_size):
-		pygame.sprite.Sprite.__init__(self)
+    def __init__(self,win_size):
+        pygame.sprite.Sprite.__init__(self)
 
-		# set separate images for moving left/right
-		# populate a circular linked list with image objects
-		#  they can be cycled-through as the octopus moves to animate it
-		self.leftImages = imageList.CircularLinkedList()
-		self.leftImages.append(pygame.image.load("images/octopus_l.png"))
-		self.leftImages.append(pygame.image.load("images/octopus_l2.png"))
-		self.leftImages.set_current() # sets a 'current image' marker on the last image in the list
-		self.rightImages = imageList.CircularLinkedList()
-		self.rightImages.append(pygame.image.load("images/octopus_r.png"))
-		self.rightImages.append(pygame.image.load("images/octopus_r2.png"))
-		self.rightImages.set_current()
-		self.image = self.rightImages.current.data # image that is displayed
-		self.rect = self.image.get_rect() # rect used for collision detection
+        # set separate images for moving left/right
+        # populate a circular linked list with image objects
+        #  they can be cycled-through as the octopus moves to animate it
+        self.leftImages = imageList.CircularLinkedList()
+        self.leftImages.append(pygame.image.load("images/octopus_l.png"))
+        self.leftImages.append(pygame.image.load("images/octopus_l2.png"))
+        self.leftImages.set_current() # sets a 'current image' marker on the last image in the list
+        self.rightImages = imageList.CircularLinkedList()
+        self.rightImages.append(pygame.image.load("images/octopus_r.png"))
+        self.rightImages.append(pygame.image.load("images/octopus_r2.png"))
+        self.rightImages.set_current()
+        self.image = self.rightImages.current.data # image that is displayed
+        self.rect = self.image.get_rect() # rect used for collision detection
 
-		self.floor = win_size[1]-self.rect[3]-1
+        self.floor = win_size[1]-self.rect[3]-1
 
-		# set starting position
-		self.x = int(win_size[0]/2) # octopus starts halfway across the screen
-		self.y = self.floor # octopus starts at the bottom of the screen
+        # set starting position
+        self.x = int(win_size[0]/2) # octopus starts halfway across the screen
+        self.y = self.floor # octopus starts at the bottom of the screen
 
-		# set rect coordinates to match image position
-		self.rect[0] = self.x
-		self.rect[1] = self.y
+        # set rect coordinates to match image position
+        self.rect[0] = self.x
+        self.rect[1] = self.y
 
-		# set starting speed (stationary)
-		self.speed = [15,0]
-		self.jump_speed = -10
+        # set starting speed (stationary)
+        self.speed = [15,0]
+        self.jump_speed = -10
 
-	def move(self):
-		# self.x += self.speed[0]
-		self.y += self.speed[1]
-		if self.speed[1] == self.jump_speed or self.speed[1] == 2: # octopus is jumping or hitting the ceiling
-			self.speed[1] = 10 # gravity
-		elif self.y >= self.floor: # octopus is hitting the floor
-			self.speed[1] = 0
+    def move(self):
+        # self.x += self.speed[0]
+        self.y += self.speed[1]
+        if self.speed[1] == self.jump_speed or self.speed[1] == 2: # octopus is jumping or hitting the ceiling
+            self.speed[1] = 10 # gravity
+        elif self.y >= self.floor: # octopus is hitting the floor
+            self.speed[1] = 0
 
-		# update Rect object position
-		self.rect[0] = self.x
-		self.rect[1] = self.y
-		# if self.rect.colliderect()
+        # update Rect object position
+        self.rect[0] = self.x
+        self.rect[1] = self.y
+        # if self.rect.colliderect()
 
-	def move_left(self):
-		# self.speed[0] = 20
-		self.x-=self.speed[0]
+    def move_left(self):
+        # self.speed[0] = 20
+        self.x-=self.speed[0]
 
-	def move_right(self):
-		# self.speed[0] = 20
-		self.x+=self.speed[0]
+    def move_right(self):
+        # self.speed[0] = 20
+        self.x+=self.speed[0]
 
-	def draw(self,surface):
-		surface.blit(self.image, (self.x, self.y))
+    def draw(self,surface):
+        surface.blit(self.image, (self.x, self.y))
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 class Platform(pygame.sprite.Sprite):
-	""" Platform the user can jump on """
+    """ Platform the user can jump on """
 
-	def __init__(self, width, height):
-		""" Platform constructor. Assumes constructed with user passing in
-			an array of 5 numbers like what's defined at the top of this code.
-			"""
-		super().__init__()
+    def __init__(self, width, height):
+        """ Platform constructor. Assumes constructed with user passing in
+            an array of 5 numbers like what's defined at the top of this code.
+            """
+        super().__init__()
 
-		self.image = pygame.Surface([width, height])
-		self.image.fill(GREEN)
+        self.image = pygame.Surface([width, height])
+        self.image.fill(GREEN)
 
-		self.rect = self.image.get_rect()
+        self.rect = self.image.get_rect()
 
 class Level():
-	""" This is a generic super-class used to define a level.
-		Create a child class for each level with level-specific
-		info. """
+    """ This is a generic super-class used to define a level.
+        Create a child class for each level with level-specific
+        info. """
 
-	def __init__(self, player, level_spec):
-		""" Constructor. Pass in a handle to player. Needed for when moving
-			platforms collide with the player. """
-		self.platform_list = pygame.sprite.Group()
-		self.enemy_list = pygame.sprite.Group()
-		self.player = player
-		self.level_spec = level_spec
+    def __init__(self, player, level_spec):
+        """ Constructor. Pass in a handle to player. Needed for when moving
+            platforms collide with the player. """
+        self.platform_list = pygame.sprite.Group()
+        self.enemy_list = pygame.sprite.Group()
+        self.player = player
+        self.level_spec = level_spec
 
-		# How far this world has been scrolled left/right
-		self.world_shift = 0
+        # How far this world has been scrolled left/right
+        self.world_shift = 0
 
-		self.level_limit = -1000
+        self.level_limit = -1000
 
-		# Go through the array above and add platforms
-		for block in level_spec:
-			self.platform_list.add(block)
+        # Go through the array above and add platforms
+        for block in level_spec:
+            self.platform_list.add(block)
 
-	# Update everythign on this level
-	def update(self):
-		""" Update everything in this level."""
-		self.platform_list.update()
-		self.enemy_list.update()
+    # Update everythign on this level
+    def update(self):
+        """ Update everything in this level."""
+        self.platform_list.update()
+        self.enemy_list.update()
 
-	def draw(self, screen):
-		""" Draw everything on this level. """
+    def draw(self, screen):
+        """ Draw everything on this level. """
 
-		# Draw all the sprite lists that we have
-		self.platform_list.draw(screen)
-		self.enemy_list.draw(screen)
+        # Draw all the sprite lists that we have
+        self.platform_list.draw(screen)
+        self.enemy_list.draw(screen)
 
-	def shift_world(self, shift_x):
-		""" When the user moves left/right and we need to scroll
-		everything: """
+    def shift_world(self, shift_x):
+        """ When the user moves left/right and we need to scroll
+        everything: """
 
-		# Keep track of the shift amount
-		self.world_shift += shift_x
+        # Keep track of the shift amount
+        self.world_shift += shift_x
 
-		# Go through all the sprite lists and shift
-		for platform in self.platform_list:
-			platform.rect.x += shift_x
+        # Go through all the sprite lists and shift
+        for platform in self.platform_list:
+            platform.rect.x += shift_x
 
-		for enemy in self.enemy_list:
-			enemy.rect.x += shift_x
+        for enemy in self.enemy_list:
+            enemy.rect.x += shift_x
 
 
 def main():
-	'''
-	Creates a pygame window with a user-controlled octopus that can move left, right, and up
-	'''
-	pygame.init()
+    '''
+    Creates a pygame window with a user-controlled octopus that can move left, right, and up
+    '''
+    pygame.init()
 
-	bg = pygame.image.load("images/undersea.png")
-	size = bg.get_size()
-	bg_rect = bg.get_rect()
-	screen = pygame.display.set_mode(size)
-	w,h = size
-	x = 0
-	y = 0
-	x1 = w
-	y1 = 0
-	# create game window
-	# white = (255, 255, 255) # color of background
-	# screen = pygame.display.set_mode((0,0),FULLSCREEN)
-	# size = screen.get_size()
+    bg = pygame.image.load("images/undersea.png")
+    size = bg.get_size()
+    bg_rect = bg.get_rect()
+    screen = pygame.display.set_mode(size)
+    w,h = size
+    x = 0
+    y = 0
+    x1 = w
+    y1 = 0
+    # create game window
+    # white = (255, 255, 255) # color of background
+    # screen = pygame.display.set_mode((0,0),FULLSCREEN)
+    # size = screen.get_size()
 
-	# create octopus object
-	octy = Octopus(size)
+    # create octopus object
+    octy = Octopus(size)
 
-	level_list = [Level(octy, spec) for spec in LEVELS_SPEC]
-	curent_level_no = 0
-	current_level = level_list[curent_level_no]
-	active_sprite_list = pygame.sprite.Group()
-	octy.level = current_level
+    level_list = [Level(octy, spec) for spec in LEVELS_SPEC]
+    curent_level_no = 0
+    current_level = level_list[curent_level_no]
+    active_sprite_list = pygame.sprite.Group()
+    octy.level = current_level
 
-	# active_sprite_list.add(octy)
+    # active_sprite_list.add(octy)
 
-	clock = pygame.time.Clock()
-	iters = 0
-	max_iters = 3 # used for animating movement -- image changes every 20 iterations
-	while True:
-		# check for events
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				sys.exit()
+    clock = pygame.time.Clock()
+    iters = 0
+    max_iters = 3 # used for animating movement -- image changes every 20 iterations
+    while True:
+        # check for events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-		pressedKeys = pygame.key.get_pressed()
+        pressedKeys = pygame.key.get_pressed()
 
-		if pressedKeys[pygame.K_ESCAPE]: # Exit
-			pygame.quit()
-			sys.exit()
-		elif pressedKeys[pygame.K_LEFT]: # Move left
-			# octy.speed[0] = -2
-			octy.move_left()
-			octy.image = octy.leftImages.current.data
-		elif pressedKeys[pygame.K_RIGHT]: # Move right
-			# octy.speed[0] = 2
-			octy.move_right()
-			octy.image = octy.rightImages.current.data
-		# else: # Stand still
-			# octy.speed[0] = 0
+        if pressedKeys[pygame.K_ESCAPE]: # Exit
+            pygame.quit()
+            sys.exit()
+        elif pressedKeys[pygame.K_LEFT]: # Move left
+            # octy.speed[0] = -2
+            octy.move_left()
+            octy.image = octy.leftImages.current.data
+        elif pressedKeys[pygame.K_RIGHT]: # Move right
+            # octy.speed[0] = 2
+            octy.move_right()
+            octy.image = octy.rightImages.current.data
+        # else: # Stand still
+            # octy.speed[0] = 0
 
-		if pressedKeys[pygame.K_UP]: # Jump upwards
-			octy.speed[1] = octy.jump_speed
+        if pressedKeys[pygame.K_UP]: # Jump upwards
+            octy.speed[1] = octy.jump_speed
 
-		# check for collisions with the edges of the window
-		if octy.x <= int(.1*size[0]):
-			octy.move_right()
-			diff = octy.rect[2]
-			current_level.shift_world(octy.speed[0])
-			x1 += octy.speed[0]
-			x += octy.speed[0]
-			if x > w:
-				x = -w
-			if x1 > w:
-				x1 = -w
-		elif octy.x + octy.rect[2] >= int(.9*size[0]):
-			octy.move_left()
-			diff = octy.rect[2]
-			current_level.shift_world(-octy.speed[0])
-			x1 -= octy.speed[0]
-			x -= octy.speed[0]
-			if x < -w:
-				x = w
-			if x1 < -w:
-				x1 = w
+        # check for collisions with the edges of the window
+        if octy.x <= int(.1*size[0]):
+            octy.move_right()
+            diff = octy.rect[2]
+            current_level.shift_world(octy.speed[0])
+            x1 += octy.speed[0]
+            x += octy.speed[0]
+            if x > w:
+                x = -w
+            if x1 > w:
+                x1 = -w
+        elif octy.x + octy.rect[2] >= int(.9*size[0]):
+            octy.move_left()
+            diff = octy.rect[2]
+            current_level.shift_world(-octy.speed[0])
+            x1 -= octy.speed[0]
+            x -= octy.speed[0]
+            if x < -w:
+                x = w
+            if x1 < -w:
+                x1 = w
 
-		if octy.y <= 0:
-			octy.speed[1] = 2
-		elif octy.y + octy.rect[3] >= size[1]:
-			octy.speed[1] = -2
+        if octy.y <= 0:
+            octy.speed[1] = 2
+        elif octy.y + octy.rect[3] >= size[1]:
+            octy.speed[1] = -2
 
-		active_sprite_list.update()
-		current_level.update()
+        active_sprite_list.update()
+        current_level.update()
 
-		# move the octopus
-		octy.move()
+        # move the octopus
+        octy.move()
 
-		# draw the background
-		screen.blit(bg,(x,y))
-		screen.blit(bg,(x1,y1))
+        # draw the background
+        screen.blit(bg,(x,y))
+        screen.blit(bg,(x1,y1))
 
-		# change octopus image for animation
-		if iters == max_iters:
-			octy.leftImages.update_current()
-			octy.rightImages.update_current()
-			iters = 0
-		else:
-			iters += 1
+        # change octopus image for animation
+        if iters == max_iters:
+            octy.leftImages.update_current()
+            octy.rightImages.update_current()
+            iters = 0
+        else:
+            iters += 1
 
-		# draw the octopus and other objects
-		disappear = {Collectible: True}.get(type(current_level.level_spec), False)
+        # draw the octopus and other objects
+        disappear = {Collectible: True}.get(type(current_level.level_spec), False)
 
-		collision_list = pygame.sprite.spritecollide(octy,
+        collision_list = pygame.sprite.spritecollide(octy,
                         current_level.platform_list, disappear)
-		for collision in collision_list:
-			collision.collision_detected()
-		screen.blit(bg,bg_rect)
-		current_level.draw(screen)
-		active_sprite_list.draw(screen)
-		octy.draw(screen)
+        for collision in collision_list:
+            collision.collision_detected()
+        screen.blit(bg,bg_rect)
+        current_level.draw(screen)
+        active_sprite_list.draw(screen)
+        octy.draw(screen)
 
-		clock.tick(60)
+        clock.tick(60)
 
-		pygame.display.flip()
+        pygame.display.flip()
 
 
 if __name__=="__main__":
-	main()
+    main()
